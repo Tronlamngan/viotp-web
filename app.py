3c72223961568bc1dd60e56dd324c67ba5b1c9f1
Tronlamngan <tronlamngan@gmail.com>
2025-06-17 11:53:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 10000))  # Render cấp port qua biến môi trường
     app.run(host="0.0.0.0", port=port)
